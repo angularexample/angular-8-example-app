f06ca5341dc9f208914500c9e6c868aabafe3850
AngularExample <info@angularexample.com>
2019-08-29 11:28:04 -0400

    header test update

--- src/app/modules/xxx-header/xxx-header.component.spec.ts
+++ src/app/modules/xxx-header/xxx-header.component.spec.ts
@@ -1,16 +1,13 @@
 import {CommonModule} from '@angular/common';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {FlexLayoutModule, MediaObserver} from '@angular/flex-layout';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-import {MockMediaObserver} from '@app/mocks/angular/mock-flex-layout';
 import {XxxHeaderComponent} from './xxx-header.component';
 import {XxxSearchBoxComponent} from '@app/modules/xxx-search-box/mock-xxx-search-box.component';
 
 describe('XxxHeaderComponent', () => {
   let component: XxxHeaderComponent;
   let fixture: ComponentFixture<XxxHeaderComponent>;
-  let mediaObserver: MediaObserver;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,11 +17,7 @@ describe('XxxHeaderComponent', () => {
       ],
       imports: [
         CommonModule,
-        FlexLayoutModule,
         MatToolbarModule
-      ],
-      providers: [
-        {provide: MediaObserver, useClass: MockMediaObserver}
       ]
     }).compileComponents();
   }));
@@ -32,7 +25,6 @@ describe('XxxHeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(XxxHeaderComponent);
     component = fixture.componentInstance;
-    mediaObserver = TestBed.get(MediaObserver);
   });
 
   it('should create', () => {
